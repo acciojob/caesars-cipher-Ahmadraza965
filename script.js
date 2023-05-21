348440594041e79627c61f440202e4968d32e993
Ahmadraza965 <120107650+Ahmadraza965@users.noreply.github.com>
2023-05-21 16:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-// Your Script here.
+// index.js
+function rot13(encodedString) {
+  // Create a helper function to shift a single character by 13 places
+  function shiftChar(char) {
+    // Convert the character to its ASCII code
+    const charCode = char.charCodeAt(0);
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+    // Check if the character is a capital letter (A-Z)
+    if (charCode >= 65 && charCode <= 90) {
+      // Apply the ROT13 shift by adding 13 to the character's code
+      let shiftedCharCode = charCode + 13;
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+      // If the shifted code goes beyond 'Z', wrap around to the beginning of the alphabet
+      if (shiftedCharCode > 90) {
+        shiftedCharCode = shiftedCharCode - 26;
+      }
 
-  return ;//return decodedArr
+      // Convert the shifted code back to a character
+      return String.fromCharCode(shiftedCharCode);
+    }
+
+    // Return non-alphabetic characters unchanged
+    return char;
+  }
+
+  // Split the encoded string into an array of characters
+  const encodedChars = encodedString.split('');
+
+  // Apply the shift to each character using the helper function
+  const decodedChars = encodedChars.map(shiftChar);
+
+  // Join the decoded characters back into a string
+  const decodedString = decodedChars.join('');
+
+  // Return the decoded string
+  return decodedString;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
+// Example usage
+const encoded = 'EBG13 rknzcyr.';
+const decoded = rot13(encoded);
+console.log(decoded);
